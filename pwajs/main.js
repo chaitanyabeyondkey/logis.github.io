967d6ba2cdd1b7f68dc53ce6ce9447baecf71a68
chaitanyabeyondkey <chaitanya.chandratre@beyondkey.com>
2022-10-06 19:02:09 +0530

    commit
    commit

--- pwajs/main.js
+++ pwajs/main.js
@@ -8,17 +8,6 @@ if('serviceWorker' in navigator){
             .then(reg => console.log('service worker: Registered'))
             .catch(err => console.log(`Service Worker : Error : ${err}`));
 
-
-            window.addEventListener('beforeinstallprompt', event => {
-                event.preventDefault();
-                var button = document.querySelector('button');
-                button.removeAttribute('hidden');
-                button.addEventListener('click', () => {
-                  event.prompt();
-                  button.setAttribute('disabled', true);
-                });
-              });
-
               
     });
 }
