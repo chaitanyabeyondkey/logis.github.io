efa264aa155771b9f51c39721660d2c21bcc3288
chaitanyabeyondkey <chaitanya.chandratre@beyondkey.com>
2022-12-08 15:45:09 +0530

    Update main.js
    commit

--- main.js
+++ main.js
@@ -16,6 +16,21 @@ if('serviceWorker' in navigator){
 /** new code 8 dec 2022 trial addeed */
 
 
+// This variable will save the event for later use.
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevents the default mini-infobar or install dialog from appearing on mobile
+  e.preventDefault();
+  // Save the event because you'll need to trigger it later.
+  deferredPrompt = e;
+  // Show your customized install prompt for your PWA
+  // Your own UI doesn't have to be a single element, you
+  // can have buttons in different locations, or wait to prompt
+  // as part of a critical journey.
+  showInAppInstallPromotion();
+});
+
+
 // Gather the data from your custom install UI event listener
 installButton.addEventListener('click', async () => {
     // deferredPrompt is a global variable we've been using in the sample to capture the `beforeinstallevent`
